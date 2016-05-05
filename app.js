d0f15a7f716a56f5f7d2fdf126e4f435711d3c28
Rachel Hamlin <rachelmhamlin@gmail.com>
2016-05-05 17:13:49 -0400

    init node app

---
+++ app.js
@@ -0,0 +1,25 @@
+var express    = require('express'),
+    morgan     = require('morgan'),
+    mongoose   = require('mongoose'),
+    bodyParser = require('body-parser'),
+    dotEnv     = ('dotenv').config();
+
+var app = express();
+
+app.use(morgan('dev'));
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'client/public/views'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(express.static('client/public'));
+
+var indexRouter = require('./server/routes/index.js')
+
+app.use('/', indexRouter);
+
+app.listen( 8080, function() {
+  console.log("project 3 on 8080");
+});
