3ac044c296c3e14857e6ba0f6974968fc38a5edc
Rachel Hamlin <rachelmhamlin@gmail.com>
2016-05-06 16:17:07 -0400

    returns results for Places search

--- client/public/js/scripts.js
+++ client/public/js/scripts.js
@@ -47,54 +47,98 @@ function initMap() {
 
   // Autocomplete search box within #search-term UI
   var input = document.getElementById('search-term');
-  var searchBox = new google.maps.places.Autocomplete(input);
-  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+  var searchBox = new google.maps.places.SearchBox(input);
 
-  // Biases results toward current map viewport
-//   map.addListener('bounds_changed', function(){
-//     searchBox.setBounds(map.getBounds());
-//   });
-// ///// FROM HERE DOWN SEPARATE INTO GET PLACES, PASS IT MAP & SEARCHBOX
-//   var markers = [];
-//   searchBox.addListener('places_changed', function() {
-//     var places = searchBox.getPlaces();
-//
-//     if (places.length == 0) {
-//       return;
-//     }
-//   })
-//
-//   markers.forEach(function(marker){
-//     marker.setMap(null);
-//   });
-//   markers = [];
-//
-//   var bounds = new.google.maps.LatLngBounds();
-//   places.forEach(function(place){
-//     var icon = {
-//       url: place.icon,
-//       size: new google.maps.Size(71, 71),
-//       origin: new google.maps.Point(0, 0),
-//       anchor: new google.maps.Point(17, 34),
-//       scaledSize: new google.maps.Size(25, 25)
-//     };
-//
-//     markers.push(new google.maps.Marker({
-//       map: map,
-//       icon: icon,
-//       title: place.name,
-//       position: place.geometry.location
-//     }));
-//
-//     if (place.geometry.viewport) {
-//       bounds.union(place.geometry.viewport);
-//     } else {
-//       bounds.extend(place.geometry.location);
-//     }
-//   });
-//
-//   map.fitBounds(bounds);
-};
+  // Bias the searchBox results toward current map viewport
+  map.addListener('bounds_changed', function(){
+    searchBox.setBounds(map.getBounds());
+  })
+
+  var markers = [];
+  // Listen for event fired when user selects a prediction and retrieve more details for that place
+  $('#search-submit').click(function() {
+    console.log('ARE YOU LISTENING TO ME');
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out old markers
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place){
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71,71),
+        origin: new google.maps.Point(0,0),
+        anchor: new google.maps.Point(17,34),
+        scaledSize: new google.maps.Size(25,25)
+      };
+
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+
+
+  // searchBox.addListener('places_changed', function(){
+  //   console.log('Selected something!');
+  //   var places = searchBox.getPlaces();
+  //
+  //   if (places.length == 0) {
+  //     return;
+  //   }
+  //
+  //   // Clear out old markers
+  //   markers.forEach(function(marker) {
+  //     marker.setMap(null);
+  //   });
+  //   markers = [];
+  //
+  //   var bounds = new google.maps.LatLngBounds();
+  //   places.forEach(function(place){
+  //     var icon = {
+  //       url: place.icon,
+  //       size: new google.maps.Size(71,71),
+  //       origin: new google.maps.Point(0,0),
+  //       anchor: new google.maps.Point(17,34),
+  //       scaledSize: new google.maps.Size(25,25)
+  //     };
+  //
+  //     markers.push(new google.maps.Marker({
+  //       map: map,
+  //       icon: icon,
+  //       title: place.name,
+  //       position: place.geometry.location
+  //     }));
+  //
+  //     if (place.geometry.viewport) {
+  //       bounds.union(place.geometry.viewport);
+  //     } else {
+  //       bounds.extend(place.geometry.location);
+  //     }
+  //   });
+  //   map.fitBounds(bounds);
+  // });
+
+
+}; // end InitMap
 
 
 
