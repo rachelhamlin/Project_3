eae7d92e2076182896face1a1a0db5fe17977b90
Irwin Tsay <Irwin@Irwins-MacBook-Air.local>
2016-05-10 20:08:54 -0400

    adds Favorite model

---
+++ server/models/favorite.js
@@ -0,0 +1,12 @@
+var mongoose        = require('mongoose');
+
+var favoriteSchema  = mongoose.Schema({
+  name:     { type: String, required: true },
+  place_id: { type: String, required: true },
+  type:     { type: String },
+  lat:      { type: Number },
+  lng:      { type: Number },
+  address:  { type: String }
+}, {timestamps: true});
+
+module.exports = mongoose.model("Favorite", favoriteSchema);
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -5,6 +5,10 @@ var jwt           = require('jsonwebtoken');
 
 var User          = require('../../models/user');
 
+// Experimental Fav stuff
+var Favorite      = require('../../models/favorite');
+// ----------------------
+
 var passport = require("../../lib/passportStrategy.js");
 
 usersRouter.post('/', function(req, res){
@@ -13,8 +17,24 @@ usersRouter.post('/', function(req, res){
     email:      req.body.email,
     password:   req.body.password,
     firstName:  req.body.firstName,
-    lastName:   req.body.lastName
+    lastName:   req.body.lastName,
+    // Experimental Fav stuff
+    favorites:  []
   });
+
+// Experimental Fav stuff
+  var newFavorite = Favorite({
+    name: "General Assembly",
+    place_id: "ChIJT3jEwaNZwokRS-hniJsDhDg",
+    type: "school",
+    lat: 40.73991760000001,
+    lng: -73.990166,
+    address: "4, 10 E 21st St, New York, NY 10010, United States"
+  });
+
+  newUser.favorites.push(newFavorite);
+
+// --------------------------
 
   newUser.save(function(error, dbUser){
     if(error){
