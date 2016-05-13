ceb4cecf799e8b1e9d7f113134223e025d76cbfa
Irwin Tsay <Irwin@Irwins-MacBook-Air.local>
2016-05-12 23:20:41 -0400

    adds spacing on Sign Up button, cleans up index.js router

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,21 +8,13 @@ var passport = require("../lib/passportStrategy");
 
 router.get('/', function(req, res) {
   var currentUser = {};
-  var theUser = {};
-  // if(req.user){
-  //   res.render('index', { current: req.user });
-  // } else {
-  //   res.render("login");
-  // }
-
   if(req.cookies.current_user) {
     currentUser = JSON.parse(req.cookies.current_user);
     var query = { username: currentUser.username };
     User.findOne(query, function(error, user){
-      theUser = user;
-      res.render('index', { currentUser: theUser });
+      // theUser = user;
+      res.render('index', { currentUser: user });
     });
-    // res.render('index', { currentUser: theUser });
   } else {
     res.render('login');
   }
